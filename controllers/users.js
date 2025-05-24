5c76cb1a9c7ef882c9a194b1ac853aac5d20b164
lukiwo0 <69887910+Lukiwo0@users.noreply.github.com>
2025-05-24 23:51:09 +0000

    melhorias no codigo

--- controllers/users.js
+++ controllers/users.js
@@ -1,36 +1,49 @@
-import { v4 as uuid } from 'uuid';
+import { v4 as uuid } from "uuid";
 
 let users = [];
 
 export const getUsers = (req, res) => {
-    console.log(`Users in the database: ${users}`);
+  res.send(users);
+};
 
-    res.send(users);
-}
+export const createUser = (req, res) => {
+  const user = req.body;
+  const newUser = { ...user, id: uuid() };
+  users.push(newUser);
 
-export const createUser = (req, res) => {   
-    const user = req.body;
-
-    users.push({...user, id: uuid()});
-    
-    console.log(`User [${user.username}] added to the database.`);
+  res.send(`User [${user.username}] added to the database.`);
 };
 
 export const getUser = (req, res) => {
-    res.send(req.params.id)
+  const { id } = req.params;
+  const foundUser = users.find((user) => user.id === id);
+
+  if (foundUser) {
+    res.send(foundUser);
+  } else {
+    res.status(404).send(`User with id ${id} not found`);
+  }
 };
 
-export const deleteUser = (req, res) => { 
-    console.log(`user with id ${req.params.id} has been deleted`);
-    
-    users = users.filter((user) => user.id !== req.params.id);
+export const deleteUser = (req, res) => {
+  const { id } = req.params;
+  users = users.filter((user) => user.id !== id);
+
+  res.send(`User with id ${id} deleted from the database.`);
 };
 
-export const updateUser =  (req,res) => {
-    const user = users.find((user) => user.id === req.params.id);
-    
-    user.username = req.body.username;
-    user.age = req.body.age;
+export const updateUser = (req, res) => {
+  const { id } = req.params;
+  const { username, age } = req.body;
 
-    console.log(`username has been updated to ${req.body.username}.age has been updated to ${req.body.age}`)
+  const user = users.find((user) => user.id === id);
+
+  if (user) {
+    if (username) user.username = username;
+    if (age) user.age = age;
+
+    res.send(`User with id ${id} has been updated.`);
+  } else {
+    res.status(404).send(`User with id ${id} not found`);
+  }
 };
